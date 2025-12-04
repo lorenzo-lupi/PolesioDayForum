48ffd23d8c0885a005a3a675528c5efae28cee21
Federico Di Petta <federico.dipetta@studenti.unicam.it>
2025-12-04 12:28:41 +0100

    add .env and app config

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,23 +4,37 @@ use utoipa_swagger_ui::SwaggerUi;
 mod models;
 mod routes;
 
+#[derive(Clone)]
+struct AppConf {
+    name: String,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::from_filename(".env.debug").ok();
     let openapi = routes::docs();
     env_logger::init();
+    
+    let conf = AppConf {
+        name: std::env::var("APP_NAME").expect("Name not set"),
+    };
+
+    log::info!("Name: {}", conf.name);
     HttpServer::new (move || {
         App::new()
         .wrap(Logger::default())
-            .service(
-                SwaggerUi::new("/swagger-ui/{_:.*}")
-                    .url("/api-docs/openapi.json", openapi.clone())
-            )
-            .service(
-                web::scope("/api")
-                    .configure(routes::init)
-            )
+        .app_data(web::Data::new(conf.clone()))
+        .service(
+            SwaggerUi::new("/swagger-ui/{_:.*}")
+                .url("/api-docs/openapi.json", openapi.clone())
+        )
+        .service(
+            web::scope("/api")
+                .configure(routes::init)
+        )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8080))
+    .expect("cannot bind on 127.0.0.1:8080")
     .run()
     .await
 }
